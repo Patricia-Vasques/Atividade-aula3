ec97cd258d2e9d779f397d856eab47b5aeee5936
Patrícia Vasques <patriciahhhh@gmail.com>
2023-03-22 21:50:04 -0300

    incluindo descrição em h1, h2 e h3

--- criando-contexto/src/components/Item.jsx
+++ criando-contexto/src/components/Item.jsx
@@ -5,9 +5,9 @@ import {UsuarioContext} from '../context/UsuarioContext'
 const usuario = useContext(UsuarioContext)
     return(
         <div>
-        <h1> {usuario.nome}</h1>
-        <h2> {usuario.idade}</h2>
-        <h3>{usuario.email}</h3>
+        <h1> O nome do usuário é {usuario.nome}</h1>
+        <h2> A idade do usuário é {usuario.idade}</h2>
+        <h3> O email do usuário é {usuario.email}</h3>
         </div>
     )
 }
